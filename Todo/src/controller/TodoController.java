f32249fa539dba3337fa138699f1f690a902c3f3
Shelby Escobedo <shelby.escobedo@gmail.com>
2016-04-24 20:45:38 -0600

    controller get user by email and password

--- Todo/src/controller/TodoController.java
+++ Todo/src/controller/TodoController.java
@@ -2,10 +2,12 @@ package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import dao.TodoDAO;
+import entities.User;
 
 @Controller
 public class TodoController {
@@ -19,4 +21,10 @@ public class TodoController {
 		return "pong";
 	}
 	
+	@ResponseBody
+	@RequestMapping("user/{email}/{password}")
+	public User getUserByEmailAndPassword
+		(@PathVariable("email") String email, @PathVariable("password") String password){
+		return todoDao.getUserByEmailAndPassword(email, password);
+	}
 }
