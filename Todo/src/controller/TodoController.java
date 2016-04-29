776561b2b97a3b4084d6d0b301ba5586cd3f6788
Shelby Escobedo <shelby.escobedo@gmail.com>
2016-04-29 13:43:11 -0600

    add user, get todo, add todo in Spring REST

--- Todo/src/controller/TodoController.java
+++ Todo/src/controller/TodoController.java
@@ -3,10 +3,12 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import dao.TodoDAO;
+import entities.Todo;
 import entities.User;
 
 @Controller
@@ -20,11 +22,29 @@ public class TodoController {
 	public String ping(){
 		return "pong";
 	}
-	
+	//getting a user based on email and password
 	@ResponseBody
 	@RequestMapping("user/{email}/{password}")
 	public User getUserByEmailAndPassword
 		(@PathVariable("email") String email, @PathVariable("password") String password){
 		return todoDao.getUserByEmailAndPassword(email, password);
+	}
+	//creating a new user
+	@ResponseBody
+	@RequestMapping("addUser")
+	public User addUser(@RequestBody User user){
+		return todoDao.createUser(user);
+	}
+	//getting a todo
+	@ResponseBody
+	@RequestMapping("todoList/{id}")
+	public Todo getTodoList(@PathVariable("id") int id){
+		return todoDao.getTodoById(id);
+	}
+	
+	@ResponseBody
+	@RequestMapping("addTodoList")
+	public Todo addTodo(@RequestBody Todo todo){
+		return todoDao.createTodo(todo);
 	}
 }
