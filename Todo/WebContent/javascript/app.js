527e1725ca5b73c4e71822332538a75d3389b82c
Shelby Escobedo <shelby.escobedo@gmail.com>
2016-05-05 14:12:24 -0600

    http not working

--- Todo/WebContent/javascript/app.js
+++ Todo/WebContent/javascript/app.js
@@ -1,6 +1,25 @@
 var app = angular.module("todoApp", []);
 
-app.controller("loginController", ["$scope",function($scope){
+app.controller("loginController", function($scope, $http){
 	
-}]);
+	
+	var getUser = function(){
+		var url = "/rest/user/"+ user.email +"/"+ user.password;
+		console.log(user);
+		$http.get(url).then(function(response){
+		$scope.loginUser = response.data;
+	});
+	var getPing = function(){
+		$http({
+			method: "GET" ,
+			url: "/rest/ping"
+			}).then(function(response){
+			console.log(response.data);
+		}, function(response){
+			console.log(response.error);
+		})
+	}
+	}
+	
+});
 
